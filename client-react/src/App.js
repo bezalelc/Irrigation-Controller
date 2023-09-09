267ca387f6607ab1d481d0e98a713173d2bd5433
Bezalel Cohen <bezalelcohen1@gmail.com>
2023-09-09 13:07:37 +0300

    init

---
+++ client-react/src/App.js
@@ -0,0 +1,14 @@
+import Home from './pages/Home';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header> </header>
+      <main><Home /></main>
+      <footer></footer>
+    </div>
+  );
+}
+
+export default App;
---
+++ client-react/src/pages/Home.js
@@ -0,0 +1,11 @@
+import style from './Home.module.scss'
+
+const Home = () => {
+    return (
+        <>
+        <p className={style.home}>Home</p>
+        </>
+        );
+    }
+    
+    export default Home;
